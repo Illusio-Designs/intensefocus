f2f8739060519e52891994c70326e24b66e67a56
Harsh Maheshwari <maheshwariharsh38@gmail.com>
2025-12-18 10:54:27 +0530

    update CORS configuration to allow all headers and add HEAD method

--- Backendjs/lib/index.js
+++ Backendjs/lib/index.js
@@ -16,8 +16,8 @@ const corsOptions = {
         'https://stallion-seven.vercel.app'
     ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'],
+    allowedHeaders: '*'
 };
 
 app.use(cors(corsOptions));
